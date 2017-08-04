69f7af705c488eeb1b87aee533a73b3c6488e709
sw-ot-prafullchandrashrimali <prafull.shrimali@softwebsolutions.com>
2017-08-04 11:01:27 +0530

    this is test

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoQA
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [Test]
+        public void Reg()
+        {
+ 
+        }
+
+    }
+}
